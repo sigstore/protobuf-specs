d0f55218f561a2a4cac47a7a078c61d057b5545d
Appu Goundan <appu@google.com>
2022-12-05 14:22:09 -0500

    Add release build for java
    Signed-off-by: Appu Goundan <appu@google.com>

--- java/README.md
+++ java/README.md
@@ -12,7 +12,10 @@ A jar file will be created at `./build/libs/protobuf-specs-<version>.jar`
 
 ## Releasing
 
-The generate jar can be released to maven central by TODO
+1. On creation of a tag in the style `release/java/v1.2.3`, new artifacts will be built and
+uploaded to a github release `release/java/v1.2.3`
+2. TODO: Explain how a releaser can then complete the release process on their machine by signing
+with pgp and uploading to maven central.
 
 ## Why is the gradle wrapper jar checked in?
 
--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,5 +1,8 @@
+import org.gradle.api.publish.maven.internal.publication.DefaultMavenPublication
+
 plugins {
     `java-library`
+    `maven-publish`
     id("com.google.protobuf") version "0.9.1"
     id("com.diffplug.spotless") version "6.11.0"
 }
@@ -11,9 +14,6 @@ sourceSets {
         }
     }
 }
-
-group = "dev.sigstore"
-version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -27,6 +27,11 @@ dependencies {
 tasks.withType<AbstractArchiveTask>().configureEach {
     isPreserveFileTimestamps = false
     isReproducibleFileOrder = true
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 protobuf {
@@ -48,4 +53,78 @@ spotless {
         endWithNewline()
     }
     // we have no non-generated java code
+}
+
+val repoUrl = "https://github.com/sigstore/protobuf-specs"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            artifactId = project.name
+            from(components["java"])
+
+            versionMapping {
+                usage(Usage.JAVA_RUNTIME) {
+                    fromResolutionResult()
+                }
+                usage(Usage.JAVA_API) {
+                    fromResolutionOf("runtimeClasspath")
+                }
+            }
+            pom {
+                name.set(
+                    (project.findProperty("artifact.name") as? String)
+                        ?: project.name
+                )
+                description.set(
+                    project.provider { project.description }
+                )
+                inceptionYear.set("2022")
+                url.set(repoUrl)
+                organization {
+                    name.set("Sigstore")
+                    url.set("https://sigstore.dev")
+                }
+                developers {
+                    developer {
+                        organization.set("Sigstore authors")
+                        organizationUrl.set("https://sigstore.dev")
+                    }
+                }
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("$repoUrl/issues")
+                }
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:$repoUrl.git")
+                    developerConnection.set("scm:git:$repoUrl.git")
+                    url.set(repoUrl)
+                    tag.set("HEAD")
+                }
+            }
+        }
+    }
+}
+
+// this task should be used by github actions to create release artifacts along with a slsa
+// attestation.
+tasks.register("createReleaseBundle") {
+    val releaseDir = layout.buildDirectory.dir("release")
+    outputs.dir(releaseDir)
+    dependsOn((publishing.publications["mavenJava"] as DefaultMavenPublication).publishableArtifacts)
+    doLast {
+        project.copy {
+            from((publishing.publications["mavenJava"] as DefaultMavenPublication).publishableArtifacts.files)
+            into(releaseDir)
+            rename("pom-default.xml", "${project.name}-${project.version}.pom")
+            rename("module.json", "${project.name}-${project.version}.module")
+        }
+    }
 }
