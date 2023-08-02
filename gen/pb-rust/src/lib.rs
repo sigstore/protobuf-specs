dcdd646b84c3759275b48f1313b6412ca699b7e4
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-08-02 12:43:38 -0400

    pb-rust: schemafy only Bundles
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>
    
    pb-rust: schemafy construction
    
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- gen/pb-rust/src/lib.rs
+++ gen/pb-rust/src/lib.rs
@@ -1,14 +1,25 @@
-use schemafy_core;
-use serde;
-use serde_json;
-
+/// NOTE(jleightcap): a method to include all JSON schemas is not immediately obvious to me:
+///
+/// - `schemafy!` is a direct 1:1 compilation to Rust structures of each definition in "defintions",
+/// - each schema is 'standalone': including definitions of all it's dependencies,
+/// as a result, the Rust generated structures have name collisions if included in the same module scope.
+///
+/// prefixing works,
+///
+/// ```rust
+/// mod CertificateIdentity {
+///   schemafy::schemafy!("schemas/CertificateIdentity.shema.json")
+/// }
+/// mod CertificateIdentities {
+///   schemafy::schemafy!("schemas/CertificateIdentities.schema.json")
+/// }
+/// ```
+/// but is clunky to use and manual to generate.
+///
+/// a more general approach is to 'flatten' each JSON Schema,
+/// unioning all of the "defintions" fields.
+///
+/// Since standardized bundles is the singular motiviation for these Rust bindings,
+/// for now we're punting this issue.
 use serde::{Deserialize, Serialize};
-
-pub mod sigstore {
-    pub mod verification {
-        pub mod v1 {
-            use super::super::super::*;
-            schemafy::schemafy!("schema/Artifact.schema.json");
-        }
-    }
-}
+schemafy::schemafy!("schemas/Bundle.schema.json");
