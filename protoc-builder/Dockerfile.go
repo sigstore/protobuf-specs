8a5102191ce3a5bbb76f22adeb74e9073556fa07
Bob Callaway <bcallaway@google.com>
2025-01-14 17:36:24 -0500

    allow each language to protoc,googleapis versions
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- protoc-builder/Dockerfile.go
+++ protoc-builder/Dockerfile.go
@@ -9,8 +9,8 @@ RUN cd tools && go build --trimpath google.golang.org/protobuf/cmd/protoc-gen-go
 FROM gcr.io/distroless/cc-debian12:nonroot@sha256:6970a2b2cb07c68f3e15d1b5d2ba857e53da911d5d321f48a842d6b0d26984cf
 
 COPY --from=go-builder /go/tools/protoc-* /usr/local/bin/
-COPY --from=protoc-base /protobuf/bin/protoc /usr/local/bin/
-COPY --from=protoc-base /protobuf/include/google /opt/include/google
-COPY --from=protoc-base /googleapis /googleapis
+COPY --from=protoc-base:go /protobuf/bin/protoc /usr/local/bin/
+COPY --from=protoc-base:go /protobuf/include/google /opt/include/google
+COPY --from=protoc-base:go /googleapis /googleapis
 
 ENTRYPOINT ["/usr/local/bin/protoc", "--plugin=protoc-gen-go=/usr/local/bin/protoc-gen-go", "--plugin=protoc-gen-go-grpc=/usr/local/bin/protoc-gen-go-grpc"]
