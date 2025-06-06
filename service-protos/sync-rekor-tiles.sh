4eac02b7f6d02d59610eef9cbe77b37067ae75e3
Appu Goundan <appu@google.com>
2025-06-06 10:17:09 -0400

    clean up sync script
    Signed-off-by: Appu Goundan <appu@google.com>

--- service-protos/sync-rekor-tiles.sh
+++ service-protos/sync-rekor-tiles.sh
@@ -13,18 +13,21 @@ if [ -z "$latest_tag" ]; then
 fi
 
 # clean up last sync
-rm ./rekor/v2/*.proto
+rm ./rekor/v2/*.proto || echo "nothing to clean up"
+
+# mkdir just in case
+mkdir -p ./rekor/v2
 
 # copy all protos over except rekor_service.proto
 echo "syncing protos with ${latest_tag}"
-git clone --filter=blob:none --no-checkout --depth=1 https://github.com/sigstore/rekor-tiles.git ./rekor-tiles && \
-    cd ./rekor-tiles && \
-    git sparse-checkout set --no-cone '/api/proto/rekor/v2/*.proto' '!**/rekor_service.proto' && \
-    git fetch origin tag "$latest_tag" --no-tags && \
-    git checkout "$latest_tag" && \
-    cd ../ && \
-    cp -R ./rekor-tiles/api/proto/* . && \
-    rm -rf ./rekor-tiles
+git clone --filter=blob:none --no-checkout --depth=1 https://github.com/sigstore/rekor-tiles.git ./rekor-tiles
+cd ./rekor-tiles
+git sparse-checkout set --no-cone '/api/proto/rekor/v2/*.proto' '!**/rekor_service.proto'
+git fetch origin tag "$latest_tag" --no-tags
+git checkout "$latest_tag"
+cd ../
+cp -R ./rekor-tiles/api/proto/* .
+rm -rf ./rekor-tiles
 
 # replace the go package from the service definition to the protobuf out
 echo "replacing go-package"
